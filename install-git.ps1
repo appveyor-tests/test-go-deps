89e1b026ac02d5c7061fa231ac773a2ae11d7802
Feodor Fitsner <feodor@appveyor.com>
2018-03-12 22:30:29 -0700

    Git 2.14.3

--- install-git.ps1
+++ install-git.ps1
@@ -1,6 +1,6 @@
-Write-Host "Installing Git 2.11.1..." -NoNewLine
-$zipPath = "$env:TEMP\PortableGit-2.11.1-64-bit.7z.exe"
-(New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.11.1.windows.1/PortableGit-2.11.1-64-bit.7z.exe', $zipPath)
+Write-Host "Installing Git 2.14.3..." -NoNewLine
+$zipPath = "$env:TEMP\PortableGit-2.14.3-64-bit.7z.exe"
+(New-Object Net.WebClient).DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.14.3.windows.1/PortableGit-2.14.3-64-bit.7z.exe', $zipPath)
 7z x $zipPath -aoa -o'C:\Program Files\Git' mingw64\bin | Out-Null
-git --version
 Write-Host "OK" -ForegroundColor Green
+git --version
